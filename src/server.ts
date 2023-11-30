573e70c22f4cdcb92357657c572281304804a0b1
Md Rakibul Hasan <hello.rakibh@gmail.com>
2023-11-30 13:35:59 +0600

    Setup completed for modular pattern

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
-import app from "./app";
-import mongoose from "mongoose";
-import config from "./app/config";
+import app from './app'
+import mongoose from 'mongoose'
+import config from './app/config'
 
 // Establish database connection
-async function main() {
+const startServer = async () => {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url as string)
 
     // Start Express server
     app.listen(config.port, () => {
-      console.log(`Server running on port: ${config.port}`);
-    });
+      console.log(`Server running on port: ${config.port}`)
+    })
   } catch (error) {
     // Log if unable to connect to database
-    console.error("Error starting server:", error);
+    console.error('Error starting server:', error)
   }
 }
 
-main();
+startServer()
